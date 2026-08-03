5719dcde44445a365c3975d619a5c89444e10592
Manu Soto <sotomedina3@gmail.com>
2026-08-03 12:59:55 -0500

    Animanu Apply
    Se incorporaron las 2 animaciones en un solo Armature. Se incorporó la Tarjeta de Tejesueños

--- Clase2/src/app.js
+++ Clase2/src/app.js
@@ -1,7 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/tarjeta.json')
+      require('../image-targets/tejesuenos.json')
     ],
   })
 }
