634125f950e64c88e9873760f98798e08d99e058
Manu Soto <sotomedina3@gmail.com>
2026-08-18 11:17:24 -0500

    TargetMultiversoUpdated

--- Clase2/src/app.js
+++ Clase2/src/app.js
@@ -1,7 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/tejesuenos.json')
+      require('../image-targets/Multiverso.json')
     ],
   })
 }
